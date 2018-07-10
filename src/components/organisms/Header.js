57664b7f340190463dcfaa62089178d295ea6455
Alex Bush <alexabush7@gmail.com>
2018-07-10 13:55:00 -0700

    starting to add media queries

--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -6,15 +6,17 @@ class Header extends Component {
   render() {
     return (
       <div className="Header div--flex-column">
-        <i className="icon--cloud fas fa-cloud" />
-        <h1 className="padding Header__h1">
-          Hi, I'm Photon, another fine little freebie from HTML5 Up.
-        </h1>
-        <p className="padding">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
-          blanditiis molestiae.
-        </p>
-        <Button title="Discover" />
+        <div className="Header__container">
+          <i className="icon--cloud fas fa-cloud" />
+          <h1 className="padding Header__h1">
+            Hi, I'm Photon, another fine little freebie from HTML5 Up.
+          </h1>
+          <p className="Header__p padding">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            blanditiis molestiae.
+          </p>
+          <Button title="Discover" />
+        </div>
       </div>
     );
   }
