a622e0e7a47d6101312fa02fc21eca7ba3f7c1ad
Alex Bush <alexabush7@gmail.com>
2018-07-10 14:27:54 -0700

    finished basic media query for Copy

--- src/components/organisms/Copy.js
+++ src/components/organisms/Copy.js
@@ -1,59 +1,56 @@
 import React, { Component } from 'react';
 
 class Copy extends Component {
-  state = {};
   render() {
     return (
       <div className="Copy">
-        <div className="Copy__background-image">
-          <div className="Copy__img-container">
-            <div className="icon__container">
-              <div className="div--diamond" />
-              <i className="icon fas fa-code" />
-            </div>
-            <div className="icon__container">
-              <div className="div--diamond" />
-              <i className="icon fas fa-bolt" />
-            </div>
-            <div className="icon__container">
-              <div className="div--diamond" />
-              <i className="icon fas fa-camera-retro" />
-            </div>
-            <div className="icon__container">
-              <div className="div--diamond" />
-              <i className="icon fas fa-cog" />
-            </div>
-            <div className="icon__container">
-              <div className="div--diamond" />
-              <i className="icon fas fa-desktop" />
-            </div>
-            <div className="icon__container">
-              <div className="div--diamond" />
-              <i className="icon fas fa-calendar-alt" />
-            </div>
+        <div className="Copy__img-container">
+          <div className="icon__container">
+            <div className="div--diamond" />
+            <i className="icon fas fa-code" />
           </div>
-          <div className="Copy__text-container">
-            <h2 className="Copy__h2">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
-              inventore odio.
-            </h2>
-            <p className="padding">
-              Quisquam aperiam at minus! Blanditiis praesentium cumque velit
-              eveniet officia amet architecto sit, enim nemo asperiores.
-              Accusamus, quos libero.
-            </p>
-            <p className="padding">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti
-              eos alias voluptas ratione ab sequi voluptatum id porro, ex,
-              deleniti nulla quisquam culpa quidem excepturi facilis fugit
-              tempora. Sunt, accusantium?
-            </p>
-            <p className="padding">
-              Quisquam aperiam at minus! Blanditiis praesentium cumque velit
-              eveniet officia amet architecto sit, enim nemo asperiores.
-              Accusamus, quos libero.
-            </p>
+          <div className="icon__container">
+            <div className="div--diamond" />
+            <i className="icon fas fa-bolt" />
           </div>
+          <div className="icon__container">
+            <div className="div--diamond" />
+            <i className="icon fas fa-camera-retro" />
+          </div>
+          <div className="icon__container">
+            <div className="div--diamond" />
+            <i className="icon fas fa-cog" />
+          </div>
+          <div className="icon__container">
+            <div className="div--diamond" />
+            <i className="icon fas fa-desktop" />
+          </div>
+          <div className="icon__container">
+            <div className="div--diamond" />
+            <i className="icon fas fa-calendar-alt" />
+          </div>
+        </div>
+        <div className="Copy__text-container">
+          <h2 className="Copy__h2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil,
+            inventore odio.
+          </h2>
+          <p className="Copy__p">
+            Quisquam aperiam at minus! Blanditiis praesentium cumque velit
+            eveniet officia amet architecto sit, enim nemo asperiores.
+            Accusamus, quos libero.
+          </p>
+          <p className="Copy__p">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti
+            eos alias voluptas ratione ab sequi voluptatum id porro, ex,
+            deleniti nulla quisquam culpa quidem excepturi facilis fugit
+            tempora. Sunt, accusantium?
+          </p>
+          <p className="Copy__p">
+            Quisquam aperiam at minus! Blanditiis praesentium cumque velit
+            eveniet officia amet architecto sit, enim nemo asperiores.
+            Accusamus, quos libero.
+          </p>
         </div>
       </div>
     );
